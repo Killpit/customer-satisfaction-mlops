7b3be5ca9f9a0bc7bbc77fc4c17ddd6a5ba0dd70
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-24 15:34:35 +0300

    Blueprint and pipeline basics

---
+++ run_pipeline.py
@@ -0,0 +1,5 @@
+from pipelines.training_pipeline import train_pipeline
+
+if __name__ == "main":
+    #Run the pipeline
+    train_pipeline(data_path = "/Users/atatekeli/PycharmProjects/customer-satisfaction-mlops/data/olist_customers_dataset.csv")
