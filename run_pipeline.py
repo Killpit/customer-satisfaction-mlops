17aaa5575ce71826cc05d6dc718676f0c224c18c
Ata Tekeli <tekeliata2000@gmail.com>
2023-08-25 17:21:26 +0300

    Fixed main method, will do the work later

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,5 @@
 from pipelines.training_pipeline import train_pipeline
 
-if __name__ == "main":
+if __name__ == "__main__":
     #Run the pipeline
     train_pipeline(data_path = "/Users/atatekeli/PycharmProjects/customer-satisfaction-mlops/data/olist_customers_dataset.csv")
